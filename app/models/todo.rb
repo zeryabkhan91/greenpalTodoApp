6639fa08d388a86bc1b21394fb7992ea4715f025
Zeryab Khan <zeryabkhan91@gmail.com>
2019-04-09 20:14:46 +0500

    todo app

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,5 @@
 class Todo < ApplicationRecord
 	validates :name, presence: true
 	validates :description, presence: true
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_091736) do
+ActiveRecord::Schema.define(version: 2018_12_06_054636) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
